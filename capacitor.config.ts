e02735d6f0e7b84187e1a905a8b515c370727a45
Angel Phiri <82968182+4anglais@users.noreply.github.com>
2026-04-23 20:59:05 +0200

    Added App Support With Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.madebyangel.nexapay',
+  appName: 'nexapay',
+  webDir: 'dist'
+};
+
+export default config;
